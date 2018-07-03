b8ceb128d2f3703767565640b6a3d5a4b0a99e16
Nicolas Deschamps de Boishebert <nicolas.ddb@gmail.com>
2018-07-03 13:48:35 +0200

    First running version.

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from flask_sqlalchemy import SQLAlchemy
-
-from mocks import Post
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////database/db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Create the model for blog posts
+class Post(db.Model):
+    """ Table Model for the blog posts. """
+
+    __tablename__ = 'Posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.title)
+
+# Return the right now datetime
 @app.context_processor
 def inject_now():
     return dict(now=datetime.now())
 
+# Return "x post(s)" with or withou the "s"
 @app.context_processor
 def utility_processor():
     def pluralize(count, singular, plural=None):
@@ -47,17 +60,23 @@ def contact():
 
 @app.route('/blog')
 def posts_index():
-    posts = Post.all()
+    posts = Post.query.all()
     return render_template('posts/index.html', posts=posts)
 
 @app.route('/blog/posts/<int:id>')
 def posts_show(id):
-    post = Post.find(id)
-    return render_template('posts/show.html', post=post)
+    post = Post.query.get(id)
+
+    if post is None:
+        abort(404)
+
+    else:
+        return render_template('posts/show.html', post=post)
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('errors/404.html'), 404
 
 if __name__ == '__main__':
+    db.create_all() # Create the database if it doesn't exist
     app.run(debug=True)
